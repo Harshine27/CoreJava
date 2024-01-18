8578bba12b10cb8684da13432fa73fa4ff38a07c
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-18 22:07:48 +0530

    Add files via upload

---
+++ DatatypesEx.java
@@ -0,0 +1,27 @@
+
+public class DatatypesEx {
+
+	public static void main(String[] args) {
+
+		byte b = 12; // byte range -128 to 127
+
+		int i = 250222;
+
+		char c = 'f'; // single char within single quotes
+
+		float f = 25.2f; // 'f' at the end
+
+		double d = 25.9;
+
+		boolean bool = true;
+
+		System.out.println(b);
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(bool);
+
+	}
+
+}
