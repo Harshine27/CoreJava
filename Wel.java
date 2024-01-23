a4eb83d9a23483da24bd28e70fc73048541e7610
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-23 18:43:47 +0530

    Add files via upload

---
+++ Wel.java
@@ -0,0 +1,20 @@
+class Wel{
+public static void main(String args[]){ // [divya 12 20 25.2 true]
+
+ String name = args[0];
+ int i = Integer.parseInt(args[1]);
+ int j = Integer.parseInt(args[2]);
+ float f = Float.parseFloat(args[3]);
+ boolean b = Boolean.parseBoolean(args[4]);
+ int sum = i+ j;
+ System.out.println(sum);
+ System.out.println(f);
+ System.out.println(b);
+ 
+
+
+}
+
+
+
+}
