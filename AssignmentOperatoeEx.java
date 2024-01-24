147dbd1361f4dd169a10b4498154f125be8e1c39
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-24 21:54:49 +0530

    Add files via upload

---
+++ AssignmentOperatoeEx.java
@@ -0,0 +1,20 @@
+
+public class AssignmentOperatoeEx {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+
+		int b = 20;
+		// a = a+ 5 // 15
+		System.out.println(a += 5);
+
+		System.out.println(b -= 10); // b = b- 10 --10
+
+		System.out.println(a *= 5);
+
+		System.out.println(b /= 5);
+
+	}
+
+}
