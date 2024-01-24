20e3ef1914e84eda3151dfe3bc2d554c8dc56cee
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-24 21:55:28 +0530

    Add files via upload

---
+++ IfElseEx.java
@@ -0,0 +1,21 @@
+
+public class IfElseEx {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+
+		int b = 20;
+
+		if (a + b < 100) {
+
+			System.out.println(" it is if block");
+
+		} else {
+
+			System.out.println(" it is else block");
+
+		}
+	}
+
+}
