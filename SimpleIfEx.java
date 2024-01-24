fac598c4522271622a67046aba51a4f4dfb77539
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-24 21:56:25 +0530

    Add files via upload

---
+++ SimpleIfEx.java
@@ -0,0 +1,17 @@
+
+public class SimpleIfEx {
+
+	public static void main(String[] args) {
+		
+		int a = 100;
+
+		if (a < 10) {
+
+			System.out.println(" a is greaterthan 10");
+
+		}
+
+		System.out.println(" it is not if block");
+	}
+
+}
