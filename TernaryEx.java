368284252d3cda83c193dcf540221502165d34fa
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-24 21:56:47 +0530

    Add files via upload

---
+++ TernaryEx.java
@@ -0,0 +1,18 @@
+
+public class TernaryEx {
+
+	public static void main(String[] args) {
+//		int a = 50;
+//		int b = 100;
+//		
+//		int min = (a<b)? a : b;
+//		System.out.println(min);
+
+		int time = 20;
+
+		String result = (time > 18) ? "Good Day " : " Good Evenning";
+
+		System.out.println(result);
+	}
+
+}
