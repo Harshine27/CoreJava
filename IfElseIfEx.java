02800b74cdd10deacf69accbfadab4d6c6888c58
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-24 21:55:46 +0530

    Add files via upload

---
+++ IfElseIfEx.java
@@ -0,0 +1,26 @@
+
+public class IfElseIfEx {
+
+	public static void main(String[] args) { 
+		
+		int a = 0;
+
+		if (a < 0) {
+			
+			System.out.println(" a is positive");
+
+		}
+
+		else if (a > 0) {
+
+			System.out.println(" a is negative");
+		}
+
+		else {
+
+			System.out.println("a is equal to zero ");
+		}
+
+	}
+
+}
