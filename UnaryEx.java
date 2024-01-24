54f5415b057382d5c20167fbb3efb8a24f56ae9a
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-24 21:57:08 +0530

    Add files via upload

---
+++ UnaryEx.java
@@ -0,0 +1,22 @@
+
+public class UnaryEx {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+
+		int b = 9;
+
+		System.out.println((a++)); // 10 11
+
+		System.out.println(a); // 11
+
+		System.out.println(--a); // 10
+
+		System.out.println(a--); // 10
+
+		System.out.println(a);
+
+	}
+
+}
