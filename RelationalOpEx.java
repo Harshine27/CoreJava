ee4848afae70f51f0e622a83c146880e9748ab6d
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-24 21:56:04 +0530

    Add files via upload

---
+++ RelationalOpEx.java
@@ -0,0 +1,20 @@
+
+public class RelationalOpEx {
+
+	public static void main(String[] args) {
+
+		int i = 100;
+
+		int j = 20;
+
+		System.out.println(i > j);
+
+		System.out.println(i < j); // false
+
+		System.out.println(i == j); // false
+
+		System.out.println(i != j); // true
+
+	}
+
+}
