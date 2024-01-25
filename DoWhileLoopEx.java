69cc089eb0045a5b22263d138375d0099f4194a7
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-25 20:13:32 +0530

    Add files via upload

---
+++ DoWhileLoopEx.java
@@ -0,0 +1,18 @@
+
+public class DoWhileLoopEx {
+
+	public static void main(String[] args) { // do while
+		
+		int i = 1;
+
+		do {
+
+			System.out.println(i); // 1 2 3 ----10
+			
+			i++;
+			
+		} while (i <= 10);
+
+	}
+
+}
