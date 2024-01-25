02e68b4f056b8341dc9857dd87600e718b556daa
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-25 20:13:14 +0530

    Add files via upload

---
+++ ContinueEx.java
@@ -0,0 +1,18 @@
+
+public class ContinueEx {
+
+	public static void main(String[] args) { // continue statement
+		
+		for (int i = 1; i <= 10; i++) {
+
+			if (i == 5) {
+
+				continue;
+
+			}
+			System.out.println(i);
+
+		}
+	}
+
+}
