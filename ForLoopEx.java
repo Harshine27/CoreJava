eb83dc267e9a2bdd894f3111de684b7b8b056a7d
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-25 20:13:50 +0530

    Add files via upload

---
+++ ForLoopEx.java
@@ -0,0 +1,15 @@
+
+public class ForLoopEx {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 20; i = i + 2) { // if i>=20 (false)
+			
+			System.out.println(i); // 0 2 4
+			
+		}
+		System.out.println(" for loop is terminated");
+
+	}
+
+}
