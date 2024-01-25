49adaf7a305269a91e70d14a3b021665b25e0989
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-01-25 20:15:18 +0530

    Add files via upload

---
+++ WhileLoopEx.java
@@ -0,0 +1,17 @@
+
+public class WhileLoopEx {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+
+		while (i <= 100) { //
+
+			System.out.println(i); // 1 2
+			
+			i++; // 2 3 101
+		}
+
+	}
+
+}
