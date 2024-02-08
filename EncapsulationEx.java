7c26e140f61684c447519b37b02cafbdcfc8aa25
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-02-08 18:36:52 +0530

    Add files via upload

---
+++ EncapsulationEx.java
@@ -0,0 +1,34 @@
+package com.corejava.oops;
+
+class Student {
+
+	private String name;
+
+	public void setName(String name) { // setter method
+
+		if (name.endsWith("a")) {
+
+			this.name = name;
+		}
+	}
+
+	public String getName() { // getter method
+
+		return name;
+	}
+
+}
+
+public class EncapsulationEx {
+
+	public static void main(String[] args) {
+
+		Student st = new Student();
+
+		st.setName("Harshine");
+		
+		System.out.println(st.getName());
+
+	}
+
+}
