a1939cb3968459e8de36d34c22760b6d6d1d22ca
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-02-09 18:36:40 +0530

    Add files via upload

---
+++ StringEx.java
@@ -0,0 +1,19 @@
+package com.corejava.string;
+
+public class StringEx {
+
+	public static void main(String[] args) {
+
+		String name = "welcome";
+
+		// + , concat()
+		
+		String result = name.concat(" to india");
+
+		//System.out.println(name.concat(" to india")); 
+		
+		System.out.println(result);
+		
+	}
+
+}
