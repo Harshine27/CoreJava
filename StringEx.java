24db44b890c600781376b87e840d9c27ef991f80
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-02-13 19:08:44 +0530

    Add files via upload

--- StringEx.java
+++ StringEx.java
@@ -5,14 +5,38 @@ public class StringEx {
 	public static void main(String[] args) {
 
 		String name = "welcome";
-
-		// + , concat()
+         // + , concat()
+		name.concat(" to india");
+        //System.out.println(name.concat(" to india")); 
+		//System.out.println(name);
 		
-		String result = name.concat(" to india");
-
-		//System.out.println(name.concat(" to india")); 
+		// by using literal
 		
-		System.out.println(result);
+		String name1 = "welcome";
+		String name2 = "welcome";
+		System.out.println(name1 == name2); // true
+		System.out.println(name1.equals(name2)); // true
+		System.out.println(name1.compareTo(name2));
+		
+		
+		// by using new keyword
+		
+		String name3 = new String("Abple");
+		String name4 = new String("bcple");
+		System.out.println(name3 == name4); // memory reference
+		System.out.println(name3.equals(name4)); // true
+		System.out.println(name3.compareTo(name4)); // values
+		System.out.println(name3.charAt(0));
+		
+		// by using StringBuffer
+		
+		StringBuffer sb1 = new StringBuffer("java");
+		StringBuffer sb2 = new StringBuffer("java");
+		sb1.append("Core");
+		System.out.println(sb1);
+		System.out.println(sb1 == sb2 ); // false
+		System.out.println(sb1.equals(sb2)); // false 
+		
 		
 	}
 
