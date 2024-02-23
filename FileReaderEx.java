71724f41d73dc2506b4b1ce672596bcbdb58600e
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-02-23 19:09:28 +0530

    Add files via upload

---
+++ FileReaderEx.java
@@ -0,0 +1,25 @@
+package com.corejava.fileio;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderEx {
+
+	public static void main(String[] args) throws IOException {
+
+		FileReader fr = new FileReader("Sample3.txt");
+
+		int i = fr.read();
+
+		while (i != -1) {
+
+			System.out.println((char) i);
+
+			i = fr.read();
+
+		}
+
+	}
+
+}
