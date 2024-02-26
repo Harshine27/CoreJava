3b44ad15107c72347df32ad50c7d10b02a7033d7
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-02-26 19:01:40 +0530

    Add files via upload

---
+++ DeSerializableEx.java
@@ -0,0 +1,22 @@
+package com.corejava.fileio;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class DeSerializableEx {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+		FileInputStream out = new FileInputStream("employees.txt");
+
+		ObjectInputStream obj = new ObjectInputStream(out);
+
+		Employee emp = (Employee) obj.readObject();
+
+		System.out.println(emp);
+
+	}
+
+}
