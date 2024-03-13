2b0031a81ff6ea4defa14034178769061edea2b7
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-03-13 19:08:50 +0530

    Add files via upload

---
+++ StaticimportsExample.java
@@ -0,0 +1,18 @@
+
+import static java.lang.System.*;
+
+import static java.lang.Math.*;
+
+public class StaticimportsExample {
+
+	public static void main(String[] args) {
+
+		System.out.println("Helllooooo java !!!!!!!!!!!"); // Math // predefined methods sqrt,power
+
+		out.println(sqrt(4));
+
+		out.println(pow(2, 3));
+
+	}
+
+}
