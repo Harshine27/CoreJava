d261247ad7fbfb9543c80dde0c6f78c7e77f049f
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-03-14 22:22:50 +0530

    Add files via upload

---
+++ AutoBoxing.java
@@ -0,0 +1,15 @@
+package com.corejava.java5;
+
+public class AutoBoxing {
+
+	public static void main(String[] args) { // autoBoxing
+		
+		int a = 10;  // primitive data type
+		
+		Integer b = a; // auto boxing(converting primitive data type into wrapper class object)
+		
+		System.out.println(a + " " + b);
+
+	}
+
+}
