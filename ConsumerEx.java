a9b42e53c3f044086857a1f198fee73da55355b3
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-03-15 19:05:57 +0530

    Add files via upload

---
+++ ConsumerEx.java
@@ -0,0 +1,23 @@
+package com.corejava.java8;
+
+import java.util.function.Consumer;
+
+//only SAM----accept()
+
+//It only consumes(takes) i/p and doesn't return anything
+
+//can be used to print information
+
+public class ConsumerEx {
+
+	public static void main(String[] args) {
+		
+		Consumer<String> c = s ->System.out.println(s);
+		
+		c.accept("Harshine");
+		
+		c.accept("divya");
+
+	}
+
+}
