4c1cac6750408732aaf3da6cedfb9fc81de54248
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-03-15 19:06:42 +0530

    Add files via upload

---
+++ FunctionEx.java
@@ -0,0 +1,23 @@
+package com.corejava.java8;
+
+import java.util.function.Function;
+
+// only SAM----apply()
+
+// any i/p ------performs operation----returns any type o/p
+
+public class FunctionEx {
+
+	public static void main(String[] args) {
+		
+		Function<String,Integer> fun = s -> s.length();
+		
+		System.out.println("The length of the String is: " + fun.apply("Divya"));
+		
+		Function<Integer,Integer> f = n->n*n;
+		
+		System.out.println("The square of the given number is:" + f.apply(5));
+
+	}
+
+}
