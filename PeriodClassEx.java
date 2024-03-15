f3c1300d245599781853859ad68ad9b45cac7455
Harshine27 <156940389+Harshine27@users.noreply.github.com>
2024-03-15 19:08:00 +0530

    Add files via upload

---
+++ PeriodClassEx.java
@@ -0,0 +1,24 @@
+package com.corejava.java8;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class PeriodClassEx {
+
+	public static void main(String[] args) {
+
+		LocalDate bdy = LocalDate.of(1999, 07, 27);
+
+		LocalDate tdy = LocalDate.now();
+
+		Period p = Period.between(bdy, tdy);
+
+		System.out.println("Years is:" + p.getYears());
+		System.out.println("Months is:" + p.getMonths());
+		System.out.println("Days is:" + p.getDays());
+
+		System.out.printf("Age is %d years  %d   months %d days", p.getYears(), p.getMonths(), p.getDays());
+
+	}
+
+}
